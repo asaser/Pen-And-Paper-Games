65e6aee4e43990e1c8436624fdfd730a7f15a636
bartlomiej <bartlomiej@euroloop.tech>
2024-06-16 19:44:26 +0200

    Add mongo DB to project

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri: string =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/your-app";
+const uri: string = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
 (async () => {
   try {
