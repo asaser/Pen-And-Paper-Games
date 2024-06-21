d704443ce3fffc664d36a059014b7404b6607049
bartlomiej <bartlomiej@euroloop.tech>
2024-06-21 16:09:06 +0200

    Uninstall eslint and install envalidate

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import env from "./util/validateEnv";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// Install additional library `npm i envalid` and `eslint` but not latest version, only 8.30 version or something similar
 
 const app: Express = express();
 
@@ -11,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri: string = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const uri = env.MONGODB_URI || "mongodb://localhost:27017";
+const port = env.PORT || 3000;
 
 (async () => {
   try {
@@ -25,8 +29,6 @@ const uri: string = process.env.MONGODB_URI || "mongodb://localhost:27017";
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).send("Server is running");
 });
-
-const port: string | number = process.env.PORT || 3000;
 
 app.listen(port!, () => {
   console.log(`Server is running on PORT: ${port}`);
