af5d79d3026a1de90f3fff5cd986887bc1dad254
bartlomiej <bartlomiej@euroloop.tech>
2024-07-19 00:23:50 +0200

    Add test model for notes

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,14 +2,17 @@ import "dotenv/config";
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 
+import NoteModel from "./models/note";
+
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_req: Request, res: Response) => {
-  res.status(200).send("Server is running");
+app.get("/", async (_req: Request, res: Response) => {
+  // res.status(200).send("Server is running");
+  const notes = await NoteModel.find().exec();
 });
 
 export default app;
