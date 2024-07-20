a777ae4d4aade824e64ad0ee7730cc016a4dff56
bartlomiej <bartlomiej@euroloop.tech>
2024-07-20 23:03:45 +0200

    Add global error handling for express not found  endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 
 import NoteModel from "./models/note";
@@ -10,9 +10,27 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (_req: Request, res: Response) => {
-  // res.status(200).send("Server is running");
-  const notes = await NoteModel.find().exec();
+app.get("/", async (_req, res, next) => {
+  try {
+    throw Error("kkkkkkk");
+
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = "Test - something wrong LOL";
+
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
