e9b820052b1892c89d87b5ff04c5bfd431a2611e
bartlomiej <bartlomiej@euroloop.tech>
2024-08-08 23:47:00 +0200

    Add user authentication and install bcrypt library

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import notesRoutes from "./routes/notes";
+import userRoutes from "./routes/users";
 import createHttpError, { isHttpError } from "http-errors";
 
 const app: Express = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/users", userRoutes);
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
---
+++ server/src/models/user.ts
@@ -0,0 +1,11 @@
+import { InferSchemaType, model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true, select: false },
+  password: { type: String, require: true, unique: true, select: false },
+});
+
+type User = InferSchemaType<typeof userSchema>;
+
+export default model<User>("User", userSchema);
