0f2da645936d248d363c0027a1ad01577dd80f47
bartlomiej <bartlomiej@euroloop.tech>
2024-08-14 21:43:36 +0200

    Add authentication for logged users

--- client/src/components/NavBarLoggedInView.tsx
+++ client/src/components/NavBarLoggedInView.tsx
@@ -3,31 +3,31 @@ import { User } from "../models/user";
 import * as NotesApi from "../network/notes_api";
 
 interface NavBarLoggedInViewProps {
-    user: User,
-    onLogoutSuccessful: () => void,
+  user: User;
+  onLogoutSuccessful: () => void;
 }
 
-const NavBarLoggedInView = ({user, onLogoutSuccessful}: NavBarLoggedInViewProps) => {
-    
-    async function logout() {
-        try {
-            await NotesApi.logout();
-            onLogoutSuccessful();
-        } catch (error) {
-            console.log(error)
-            alert(error)
-        }
+const NavBarLoggedInView = ({
+  user,
+  onLogoutSuccessful,
+}: NavBarLoggedInViewProps) => {
+  async function logout() {
+    try {
+      await NotesApi.logout();
+      onLogoutSuccessful();
+    } catch (error) {
+      console.log(error);
+      alert(error);
     }
-    return (
-        <>
-            <input style={{color: "#FFF"}}>
-                Signed in as: {user.username}
-            </input>
-            <Button onClick={logout} sx={{ my: 2, color: 'white', display: 'block' }}>
-                Log out
-            </Button>
-        </>
-    );
-}
- 
+  }
+  return (
+    <>
+      <p style={{ color: "#FFF" }}>Signed in as: {user.username}</p>
+      <Button onClick={logout} sx={{ my: 2, color: "white", display: "block" }}>
+        Log out
+      </Button>
+    </>
+  );
+};
+
 export default NavBarLoggedInView;
