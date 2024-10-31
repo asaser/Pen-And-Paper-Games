75b1213a935fbee3e171deb786f0332584089a4d
bartlomiej <bartlomiej@euroloop.tech>
2024-10-31 21:40:13 +0100

    Fix  button text from 'sigh up' to sign up'

--- client/src/components/NavBarLoggedOutView.tsx
+++ client/src/components/NavBarLoggedOutView.tsx
@@ -1,21 +1,30 @@
 import { Button } from "@mui/material";
 
 interface NavBarLoggedOutViewProps {
-    onSignUpClicked: () => void,
-    onLoginClicked: () => void,
+  onSignUpClicked: () => void;
+  onLoginClicked: () => void;
 }
 
-const NavBarLoggedOutView = ({ onSignUpClicked, onLoginClicked}: NavBarLoggedOutViewProps) => {
-    return (
-        <>
-            <Button onClick={onSignUpClicked} sx={{ my: 2, color: 'white', display: 'block' }}>
-                Sigh Up
-            </Button>
-            <Button onClick={onLoginClicked} sx={{ my: 2, color: 'white', display: 'block' }}>
-                Log In
-            </Button>
-        </>
-    );
-}
- 
+const NavBarLoggedOutView = ({
+  onSignUpClicked,
+  onLoginClicked,
+}: NavBarLoggedOutViewProps) => {
+  return (
+    <>
+      <Button
+        onClick={onSignUpClicked}
+        sx={{ my: 2, color: "white", display: "block" }}
+      >
+        Sign Up
+      </Button>
+      <Button
+        onClick={onLoginClicked}
+        sx={{ my: 2, color: "white", display: "block" }}
+      >
+        Log In
+      </Button>
+    </>
+  );
+};
+
 export default NavBarLoggedOutView;
